d1758f57f76071f7c7f059de48254d9cef163a3b
Artur Sousa <artursousa505@gmail.com>
2021-01-16 08:43:13 -0300

    user update

--- src/main/java/com/example/webStart/services/UserService.java
+++ src/main/java/com/example/webStart/services/UserService.java
@@ -30,4 +30,17 @@ public class UserService {
 	public void delete(Long id) {
 		userRepository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		//getOne starts the object and JPA monitors it so we can perform operations on the DB
+		User entity = userRepository.getOne(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 }
